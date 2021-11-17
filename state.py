05029a36801d7345b1b44d8a345a4c7b705dd7ed
monokeith <hgyixg@gmail.com>
2021-11-17 11:34:34 -0500

    a very very very simple reflex agent

--- state.py
+++ state.py
@@ -5,64 +5,6 @@ from resources import ImageResources, SoundResource
 from pygame.locals import *
 
 
-class Bird:
-    # down is positive, up is negative
-    FLAP_ACC = -9
-    ACCELERATION_Y = 1
-    VELOCITY_Y_MAX = 10
-    ROTATION_SPEED = 3
-
-    def __init__(self, state):
-        self.height_limit = -2 * state.images.player[0].get_height()
-        self.game_state = state
-
-        # Location related
-        self.x = 0
-        self.y = 0
-        self.rotation = 45
-        self.velocity_y = -9
-        # If bird flapped in current frame
-        self.flapped = False
-
-    def get_x(self):
-        return self.x
-
-    def get_y(self):
-        return self.y
-
-    def get_velocity_y(self):
-        return self.velocity_y
-
-    # gets call every frame
-    def run(self):
-        # Flap?
-        for event in pygame.event.get():
-            if event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
-                self.flap()
-        # Refresh
-        self.refresh_location()
-
-    # Flap
-    def flap(self):
-        if self.y > self.height_limit:
-            self.game_state.sounds.wing.play()
-            self.velocity_y = Bird.FLAP_ACC
-            self.flapped = True
-
-    # Update location every frame
-    def refresh_location(self):
-        # Rotate
-        if self.rotation > -90:
-            self.rotation -= Bird.ROTATION_SPEED
-        # Height of bird drop
-        if self.velocity_y < Bird.VELOCITY_Y_MAX and not self.flapped:
-            self.velocity_y += Bird.ACCELERATION_Y
-        # Flap update
-        if self.flapped:
-            self.rotation = 45
-            self.flapped = False
-
-
 class State:
     # Constants
     FPS = 30
@@ -109,3 +51,78 @@ class State:
         pipe_x = (State.SCREEN_WIDTH + 10) if x == 0 else x
         self.upper_pipes.append({'x': pipe_x, 'y': gap_y - self.pipe_height})
         self.lower_pipes.append({'x': pipe_x, 'y': gap_y + State.PIPE_GAP_SIZE})
+
+
+class Bird:
+    # down is positive, up is negative
+    FLAP_ACC = -9
+    ACCELERATION_Y = 1
+    VELOCITY_Y_MAX = 10
+    ROTATION_SPEED = 3
+
+    def __init__(self, state: State):
+        self.height_limit = -2 * state.images.player[0].get_height()
+        self.game_state = state
+
+        # Location related
+        self.x = 0
+        self.y = 0
+        self.rotation = 45
+        self.velocity_y = -9
+        # If bird flapped in current frame
+        self.flapped = False
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def get_velocity_y(self):
+        return self.velocity_y
+
+    # gets call every frame
+    def run(self):
+        self.simple_reflex_agent()
+        # Refresh
+        self.refresh_location()
+
+    # A simple reflex agent to automatically flap
+    def simple_reflex_agent(self):
+        offset_allowance = 35
+        y_upper = self.game_state.upper_pipes[0]['y'] + offset_allowance
+        y_lower = self.game_state.lower_pipes[0]['y'] - offset_allowance
+
+        if self.y >= y_lower:
+            self.log("Agent flap")
+            self.flap()
+
+
+    def keyboard_bird(self):
+        # Flap?
+        for event in pygame.event.get():
+            if event.type == KEYDOWN and (event.key == K_SPACE or event.key == K_UP):
+                self.flap()
+
+    # Flap
+    def flap(self):
+        if self.y > self.height_limit:
+            self.game_state.sounds.wing.play()
+            self.velocity_y = Bird.FLAP_ACC
+            self.flapped = True
+
+    # Update location every frame
+    def refresh_location(self):
+        # Rotate
+        if self.rotation > -90:
+            self.rotation -= Bird.ROTATION_SPEED
+        # Height of bird drop
+        if self.velocity_y < Bird.VELOCITY_Y_MAX and not self.flapped:
+            self.velocity_y += Bird.ACCELERATION_Y
+        # Flap update
+        if self.flapped:
+            self.rotation = 45
+            self.flapped = False
+
+    def log(self, msg):
+        print("[Bird] %s" % msg)
